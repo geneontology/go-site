bb01524119f58f6914267556b8ec5e4c42b2452f
SC <github.com@genkisugi.net>
2014-07-16 13:42:13 -0700

    Prevent accidental saving of not-so-good JSON files.

---
+++ scripts/json-syntax-check.el
@@ -0,0 +1,25 @@
+;;;;
+;;;; Loading this file should prevent accidentally saving a JSON file
+;;;; with incorrect syntax.
+;;;;
+
+;; This should be in most modern emacs.
+(require 'json)
+
+;; Check that the JSON syntax is correct in the current buffer by
+;; reading it in and parsing it.
+(defun json-current-buffer-syntax-check ()
+  (interactive)
+  (condition-case nil
+      (if (json-read-from-string (buffer-string)) t nil)))
+
+;; When in JSON mode, add a /local/ before-save-hook that runs our
+;; command.
+(add-hook 'json-mode-hook
+	  (lambda ()
+	    (add-hook 'before-save-hook
+		      (lambda ()			
+			(when (string-match "\\.json$" ; don't need this?
+					    (buffer-name (current-buffer)))
+			  (json-current-buffer-syntax-check)))
+		      nil t)))
