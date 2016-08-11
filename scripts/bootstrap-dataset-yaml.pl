1cc2c180e4dfa3d41b61f94c722b323bf796e9db
Chris Mungall <cjm@berkeleybop.org>
2016-08-11 13:59:46 -0700

    split metadata to individual by-authority files

--- scripts/bootstrap-dataset-yaml.pl
+++ scripts/bootstrap-dataset-yaml.pl
@@ -37,12 +37,82 @@ my %taxon2db = (
     'taxon:9606' => 'goa_human',
     'taxon:9823' => 'goa_pig',
     'taxon:9615' => 'goa_dog',
+    'taxon:4081' => 'sgn',
+
+    'taxon:176299' => 'PAMGO_Atumefaciens',
+
+    'taxon:123356' => 'PAMGO_Oomycetes',
+    'taxon:4784' => 'PAMGO_Oomycetes',
+    'taxon:4785' => 'PAMGO_Oomycetes',
+    'taxon:4786' => 'PAMGO_Oomycetes',
+    'taxon:4787' => 'PAMGO_Oomycetes',
+    'taxon:4792' => 'PAMGO_Oomycetes',
+    'taxon:65070' => 'PAMGO_Oomycetes',
+    'taxon:67593' => 'PAMGO_Oomycetes',
+    'taxon:148305' => 'PAMGO_Mgrisea',
+    'taxon:198628' => 'PAMGO_Ddadantii',
+
+    'taxon:5833' => 'GeneDB_Pfalciparum',
+    'taxon:37546' => 'GeneDB_tsetse',
+    'taxon:212042' => 'jcvi',
+    'taxon:198094' => 'jcvi',
+    'taxon:227377' => 'jcvi',
+    'taxon:246194' => 'jcvi',
+    'taxon:195099' => 'jcvi',
+    'taxon:195103' => 'jcvi',
+    'taxon:167879' => 'jcvi',
+    'taxon:243164' => 'jcvi',
+    'taxon:205920' => 'jcvi',
+    'taxon:243231' => 'jcvi',
+    'taxon:228405' => 'jcvi',
+    'taxon:265669' => 'jcvi',
+    'taxon:243233' => 'jcvi',
+    'taxon:222891' => 'jcvi',
+    'taxon:220664' => 'jcvi',
+    'taxon:223283' => 'jcvi',
+    'taxon:264730' => 'jcvi',
+    'taxon:211586' => 'jcvi',
+    'taxon:246200' => 'jcvi',
+    'taxon:686' => 'jcvi',
+    'taxon:4528' => 'gramene_oryza',
+    'taxon:4529' => 'gramene_oryza',
+    'taxon:4530' => 'gramene_oryza',
+    'taxon:4532' => 'gramene_oryza',
+    'taxon:4533' => 'gramene_oryza',
+    'taxon:4534' => 'gramene_oryza',
+    'taxon:4535' => 'gramene_oryza',
+    'taxon:4536' => 'gramene_oryza',
+    'taxon:4537' => 'gramene_oryza',
+    'taxon:4538' => 'gramene_oryza',
+    'taxon:29689' => 'gramene_oryza',
+    'taxon:29690' => 'gramene_oryza',
+    'taxon:39946' => 'gramene_oryza',
+    'taxon:39947' => 'gramene_oryza',
+    'taxon:40148' => 'gramene_oryza',
+    'taxon:40149' => 'gramene_oryza',
+    'taxon:52545' => 'gramene_oryza',
+    'taxon:63629' => 'gramene_oryza',
+    'taxon:65489' => 'gramene_oryza',
+    'taxon:65491' => 'gramene_oryza',
+    'taxon:77588' => 'gramene_oryza',
+    'taxon:83307' => 'gramene_oryza',
+    'taxon:83308' => 'gramene_oryza',
+    'taxon:83309' => 'gramene_oryza',
+    'taxon:110450' => 'gramene_oryza',
+    'taxon:110451' => 'gramene_oryza',
+    'taxon:127571' => 'gramene_oryza',
+    'taxon:364099' => 'gramene_oryza',
+    'taxon:364100' => 'gramene_oryza',
+    'taxon:208964' => 'pseudocap',
+    
     );
 
 open(F,'db-xrefs.yaml') || die;
 my $yaml = join("",<F>);
 close(F);
 my $xrefs = Load($yaml);
+
+my %is_append = ();
 
 push(@$xrefs, {database=>'GOA', name=>'EBI Gene Ontology Annotation Database'});
 
@@ -55,8 +125,16 @@ foreach (@ARGV) {
     unless (m@\.gz$@) {
         next;
     }
+
+    ## Deprecated
+    next if m@goa_ref@;
+
+    ## Old form: replaced by goa_foo[_type].gaf.gz
+    next if m@gene_association\.goa@;
+
     # stale MGIs
     next if m@association.*.gpi.gz@;
+
     if (m@gene_association.(\S+).gz@) {
         emit('gaf', $1, $_);
     }
@@ -64,7 +142,7 @@ foreach (@ARGV) {
         emit('gpi', $1, $_);
     }
     elsif (m@(\w+)_association.gpa.gz@) {
-        emit('gpi', $1, $_);
+        emit('gpad', $1, $_);
     }
     elsif (m@(\w+).gpi.gz@) {
         emit('gpi', $1, $_);
@@ -76,6 +154,16 @@ foreach (@ARGV) {
         emit('gpad', $1, $_);
     }
 }
+
+foreach my $f (keys %is_append) {
+    my $bn = $f;
+    $bn =~ s@.yaml@.json@;
+    my $cmd = "yaml2json.pl $f > $bn";
+    if (system($cmd)) {
+        die $cmd;
+    }
+}
+
 exit 0;
 
 sub emit {
@@ -95,6 +183,9 @@ sub emit {
         }
         elsif ($dataset =~ m@(\w+)_(\w+)@) {
             $auth = $1;
+            if ($auth eq 'goa') {
+                $entity_type = 'protein';
+            }
         }
         else {
             warn $dataset;
@@ -125,7 +216,7 @@ sub emit {
     
     my @taxids = ();
     foreach my $k (keys %taxon2db) {
-        if ($subdb eq $taxon2db{$k}) {
+        if ($subdb eq lc($taxon2db{$k})) {
             my $t = $k;
             $t =~ s@taxon:@NCBITaxon:@;
             push (@taxids, $t);
@@ -135,8 +226,21 @@ sub emit {
         print STDERR "NO TAXA FOR '$subdb' (dataset=$dataset)\n";
     }
     my $TAXA = join("", (map {"   - $_\n"} @taxids));
+
+    my $ofn = "datasets/$auth.yaml";
+    if ($is_append{$ofn}) {
+        open(F, ">>$ofn") || die $ofn;
+    }
+    else {
+        open(F, ">$ofn") || die $ofn;
+        $is_append{$ofn}++;        
+    }
+    my $status = 'active';
+    if ($auth eq 'reactome' || $auth eq 'goa_pdb') {
+        $status = "inactive";
+    }
     
-    print <<EOM;
+    print F <<EOM;
 -    
  id: $id
  label: "$dataset $type file"
@@ -148,9 +252,12 @@ sub emit {
  compression: gzip
  source: $src
  entity_type: $entity_type
+ status: $status
  taxa:
 $TAXA
 EOM
+
+   close(F);
 }
 
 
