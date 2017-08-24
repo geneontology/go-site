e6ff6f1b24c88996732a06ed0a5e1832b3572804
Eric Douglass <edouglass@lbl.gov>
2017-08-24 16:35:07 -0700

    datasetes skipped for some reason will no longer be expected to provide a ttl target

--- pipeline/util/generate-makefile.py
+++ pipeline/util/generate-makefile.py
@@ -10,7 +10,7 @@ import argparse
 import yaml
 from json import dumps
 
-SKIP = ["goa_pdb"]
+SKIP = ["goa_pdb", "goa_uniprot_all", "goa_uniprot_gcrp"]
 
 def main():
 
@@ -53,7 +53,7 @@ def main():
     targets = [all_files(ds) for ds in artifacts_by_dataset.keys()]
     rule('all_targets', targets)
 
-    simple_ds_list = [ds for ds in artifacts_by_dataset.keys() if ds != 'goa_uniprot_all' and ds != 'goa_uniprot_gcrp' and ds not in SKIP]
+    simple_ds_list = [ds for (ds, data) in artifacts_by_dataset.items() if not skip_source(ds, data)]
     simple_targets = [all_files(ds) for ds in simple_ds_list]
     rule('all_targets_simple', simple_targets, comments='Excludes aggregated (goa_uniprot)')
 
@@ -108,6 +108,9 @@ def generate_targets(ds, alist):
         rule(src_gaf_zip(ds),[targetdir(ds)],
              'wget --no-check-certificate {url} -O $@.tmp && mv $@.tmp $@ && touch $@'.format(url=url))
 
+def skip_source(ds, data):
+    types = [a['type'] for a in data]
+    return ds in SKIP or ('gaf' not in types and 'gpad' not in types)
 
 def create_targetdir(ds):
     return 'create_targetdir_'+ds
@@ -117,8 +120,6 @@ def all_files(ds):
     return 'all_'+ds
 def all_ttl(ds):
     return 'ttl_all_'+ds
-def all_asserted_ttl(ds):
-    return 'ttl_asserted_all_'+ds
 def src_gaf_zip(ds):
     return '{dir}{ds}-src.gaf.gz'.format(dir=targetdir(ds),ds=ds)
 def src_gaf(ds):
