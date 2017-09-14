8aff7bd393a4dd76551662b3cf139e40f6c4c6c7
Eric Douglass <edouglass@lbl.gov>
2017-09-14 14:21:26 -0700

    adding sparta to makefile targets

--- pipeline/triplestore-rulecheck.sh
+++ pipeline/triplestore-rulecheck.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 ENDPOINT=http://localhost:$1/blazegraph/sparql
+JOURNAL=${2:-"target/blazegraph-internal.jnl"}
+OUT=${3:-"target/rules.json"}
 
 function bg_start() {
-    java -server -Xmx32g -Djetty.port=$1 -Djetty.overrideWebXml=conf/readonly_cors.xml -Dbigdata.propertyFile=conf/blazegraph.properties -cp target/jars/blazegraph-jar.jar:target/jars/jetty-servlets.jar com.bigdata.rdf.sail.webapp.StandaloneNanoSparqlServer &
+    java -server -Xmx32g -Djetty.port=$1 \
+        -Djetty.overrideWebXml=conf/readonly_cors.xml \
+        -Dbigdata.propertyFile=conf/blazegraph.properties \
+        -Dcom.bigdata.journal.AbstractJournal.file=$JOURNAL \
+        -cp target/jars/blazegraph-jar.jar:target/jars/jetty-servlets.jar com.bigdata.rdf.sail.webapp.StandaloneNanoSparqlServer &
 }
 
 bg_start $1
 export BG_PID=$!
 echo "Wait for blazegraph to start..."
 sleep 3
-sparta group $ENDPOINT --out target/rules.json
+sparta group $ENDPOINT --out $OUT
 echo "Killing blazegraph on $BG_PID"
 kill $BG_PID
