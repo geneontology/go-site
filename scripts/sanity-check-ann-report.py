a4fdf47fc00a4c9716d3453e085ce46ff1c764c5
SC <github.com@genkisugi.net>
2017-10-24 20:45:10 -0700

    tuned sanity checks to work on smaller files; fixed logging options

--- scripts/sanity-check-ann-report.py
+++ scripts/sanity-check-ann-report.py
@@ -5,11 +5,12 @@
 ####
 #### Example usage to analyze "whatever":
 ####  python3 sanity-check-ann-report.py --help
+####  mkdir -p /tmp/mnt || true
 ####  mkdir -p /tmp/foo || true
 ####  sshfs -oStrictHostKeyChecking=no -o IdentitiesOnly=true -o IdentityFile=/home/sjcarbon/local/share/secrets/bbop/ssh-keys/foo.skyhook -o idmap=user skyhook@skyhook.berkeleybop.org:/home/skyhook /tmp/mnt/
 ####  cp /tmp/mnt/master/annotations/whatever* /tmp/foo
 ####  fusermount -u /tmp/mnt
-####  python3 sanity-check-ann-report.py -v -d /tmp/foo/
+####  python3 sanity-check-ann-report.py -v -d /tmp/foo
 ####
 
 import sys
@@ -20,9 +21,14 @@ import os
 import re
 import subprocess
 
+## Logger basic setup.
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('sanity')
+logger.setLevel(logging.WARNING)
+
 ## Make sure we exit in a way that will get Jenkins's attention.
 def die_screaming(str):
-    logging.error(str)
+    logger.error(str)
     sys.exit(1)
 
 def main():
@@ -42,17 +48,23 @@ def main():
     regexp_lines_assocs = re.compile(r'Associations: (.*)$', re.MULTILINE)
     regexp_lines_fatal = re.compile(r'FATAL: (.*)$', re.MULTILINE)
     
+    ## Get logger doing something.
+    
+    if args.verbose:
+        logger.setLevel(logging.INFO)
+        logger.info('Verbose: on')
+
     ## Ensure directory.
     if not args.directory:
         die_screaming('need a directory argument')
-    logging.info('Will operate in: ' + args.directory)
+    logger.info('Will operate in: ' + args.directory)
 
     ids = []
     
     ## Get files out of target directory, searching for the IDs
     ## independent of the metadata (as we'll need to check that too).
     src_filenames = glob.glob(args.directory + '/*-src.*')
-    logging.info(src_filenames)
+    logger.info(src_filenames)
     for src_filename in src_filenames:
         ## Chop off all extensions.
         potential_id = src_filename
@@ -74,9 +86,14 @@ def main():
     lookup = {}
     for aid in ids:
 
+        # if aid == 'goa_uniprot_all':
+        #     break
+        
         ###
         ### Extract information from the report.
         ###
+        
+        logger.info("Processing: " + aid)
         
         ## Read.
         ## WARNING: Using the markdown version is a holdover from when
@@ -85,7 +102,7 @@ def main():
         with open(args.directory + '/' + aid + '.report.md') as f:
             read_data = f.read()
         f.closed
-
+        
         ## Better be something in there.
         if not read_data:
             die_screaming('No report found for :' + aid)
@@ -115,27 +132,32 @@ def main():
         lines_fatal = int(res[0])
 
         ###
-        ### Extract information from actual.
+        ### Extract information from actual using grep.        
+        ###  grep EXIT STATUS
+        ###  The following exit values are returned:
+        ###  0 One or more matches were found.
+        ###  1 No matches were found.
+        ###  2 Syntax errors or inaccessible files (even if matches were found).
         ###
 
         ## Get source count.
         foo = subprocess.run(
             'zgrep -Ec "$" ' + args.directory + '/' + aid + '-src.gaf.gz',
-            shell=True, check=True, stdout=subprocess.PIPE)
+            shell=True, check=False, stdout=subprocess.PIPE)
         if type(foo) is not subprocess.CompletedProcess:
             die_screaming('Shell fail on: ' + str(foo))
-        elif foo.returncode != 0:
-            die_screaming('Bad return on: ' + str(foo))
+        elif foo.returncode == 2:
+            die_screaming('Bad file on: ' + str(foo))
         count_gaf_src = int(foo.stdout)
         
         ## Get product count.
         foo = subprocess.run(
             'zgrep -Ec "$" ' + args.directory + '/' + aid + '.gaf.gz',
-            shell=True, check=True, stdout=subprocess.PIPE)
+            shell=True, check=False, stdout=subprocess.PIPE)
         if type(foo) is not subprocess.CompletedProcess:
             die_screaming('Shell fail on: ' + str(foo))
-        elif foo.returncode != 0:
-            die_screaming('Bad return on: ' + str(foo))
+        elif foo.returncode == 2:
+            die_screaming('Bad file on: ' + str(foo))
         count_gaf_prod = int(foo.stdout)
         
         ## Assemble report object.
@@ -152,8 +174,7 @@ def main():
                 'gaf-product': count_gaf_prod
             }
         }
-    logging.info(lookup)
-    #print(lookup)
+    logger.info(lookup)
 
     ###
     ### Final QC crunching.
@@ -173,11 +194,16 @@ def main():
         ### Checks.
         ###
 
-        ## There must be a product.
-        if count_gaf_prod == 0:
+        ## There must be a product when something comes in.
+        ## Keep in mind that the src files sometimes have header
+        ## comments, but no content, so we give a buffer of 8.
+        if count_gaf_prod == 0 and count_gaf_src > 8:
+            logger.warning('count_gaf_src ' + str(count_gaf_src))
+            logger.warning('count_gaf_prod ' + str(count_gaf_prod))
             die_screaming('No product found for: ' + aid)
-        ## Product must not be a "severe" reduction from source.
-        if count_gaf_prod < (count_gaf_src / 2):
+        ## Product must not be a "severe" reduction from source, but
+        ## only in cases of larger files.
+        if (count_gaf_prod < (count_gaf_src / 2)) and count_gaf_src > 25:
             die_screaming('Severe reduction of product for: ' + aid)
         ## No fatal remarks should have been made.
         if lines_fatal > 0:
