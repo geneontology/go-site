0b3f6981c71377767dcaf156b3aa108aa7e7e79e
Eric Douglass <edouglass@lbl.gov>
2018-01-08 15:04:36 -0800

    paint merge script more robust to missing files with a nice warning message, but keeps going

--- pipeline/util/paint_merge.py
+++ pipeline/util/paint_merge.py
@@ -29,6 +29,14 @@ def merges_into_path(merges_into, groups_dir):
     return os.path.abspath(os.path.join(groups_dir, merges_into, merge_into_gaf))
 
 def append_zip_into_zip(merger, merge_into):
+    if not os.path.exists(merger):
+        click.echo(click.style("{} does not exist, skipping".format(merger), fg="red"), err=True)
+        return
+
+    if not os.path.exists(merge_into):
+        click.echo(click.style("{} does not exist, skipping".format(merge_into), fg="red"), err=True)
+        return
+
     base, leaf = os.path.split(merge_into)
     merged_leaf = leaf.split(".gaf.gz")[0] + "_merged.gaf.gz"
     final = os.path.join(base, merged_leaf)
