2acce1de552cb4a636a485b15cceb2c0a19319d7
Eric Douglass <edouglass@lbl.gov>
2018-01-08 22:33:34 -0800

    removing sgd from iteration branch

--- pipeline/util/generate-makefile.py
+++ pipeline/util/generate-makefile.py
@@ -11,7 +11,7 @@ import yaml
 from json import dumps
 
 SKIP = ["goa_pdb", "goa_uniprot_gcrp"]
-ONLY_INCLUDE = ["goa_uniprot_all", "mgi", "sgd", "wb", "paint_mgi", "paint_sgd", "paint_wb", "goa_chicken_complex"]
+ONLY_INCLUDE = ["goa_uniprot_all", "mgi", "wb", "paint_mgi", "paint_wb", "goa_chicken_complex"]
 ONLY_GAF = []
 
 def main():
