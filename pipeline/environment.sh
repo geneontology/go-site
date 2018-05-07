a982c8372418a3664597bb8157a8d24fecb5e7b5
Eric Douglass <edouglass@lbl.gov>
2018-05-07 14:30:49 -0700

    updating requirements and environment in pipeline

--- pipeline/environment.sh
+++ pipeline/environment.sh
@@ -2,6 +2,6 @@
 python3 -m venv target/env
 . target/env/bin/activate
 
-pip3 install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple  -r requirements.txt
+pip3 install -r requirements.txt
 
 pip3 install ../graphstore/rule-runner
