4dd681fe9d9a593b3332c80ee62938daf8ed0658
Eric Douglass <dougli1sqrd@gmail.com>
2019-07-26 14:50:42 -0700

    why does blazegraph die

--- pipeline/triplestore-rulecheck.sh
+++ pipeline/triplestore-rulecheck.sh
@@ -15,6 +15,7 @@ bg_start $1
 export BG_PID=$!
 echo "Wait for blazegraph to start..."
 sleep 3
-$PY_ENV/bin/python3 $PY_ENV/bin/sparta group $ENDPOINT --out $OUT
+echo "skip sparta for now..."
+# $PY_ENV/bin/python3 $PY_ENV/bin/sparta group $ENDPOINT --out $OUT
 echo "Killing blazegraph on $BG_PID"
 kill $BG_PID
