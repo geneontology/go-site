b8caaf1aa3ea2a6f674d2805b70529cbed13ef61
Eric Douglass <dougli1sqrd@gmail.com>
2020-06-09 15:12:20 -0700

    makefile for gaferencer reprocess

--- scripts/gaferencer-reprocess.sh
+++ scripts/gaferencer-reprocess.sh
@@ -8,9 +8,8 @@ python3 /opt/go-site/scripts/json-concat-lists.py /opt/go-site/gaferencer-produc
 # Uncompress all files in annotations/.
 unpigz /opt/go-site/annotations/*.gz
 # This ould provide absolute paths in $f for all files in /tmp/annotations. Just plug in 'ontobio-parse-assocs' cmd
-# by replacing 'echo $f'.
-for f in /opt/go-site/annotations/* ; do ontobio-parse-assocs.py -f $f -F gaf -o /opt/go-site/annotations_new/$(basename $f) -I /opt/go-site/gaferencer-products/all.gaferences.json --report-md /tmp/report.md --report-json /tmp/report.json validate; done
-
+# by replacing 'echo $f'
+make -f /opt/go-site/scripts/Makefile-gaf-reprocess
 # After ontobio-parse-assocs is run and we're all
 # done, gzip up all.gaferences.json, all
 # annotations_new/ files, and then upload.
