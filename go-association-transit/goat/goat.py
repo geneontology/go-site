27655385c15990c702b9939b523accf7e6f857ba
dougli1sqrd <dougli1sqrd+git@gmail.com>
2021-02-05 11:35:53 -0800

    goat will parse files into a gpad 2.0

--- go-association-transit/goat/goat.py
+++ go-association-transit/goat/goat.py
@@ -1,6 +1,11 @@
+#!/usr/env/bin python3
+
 import click
 import os
 
+from typing import List
+
+from goat import sources
 from goat import parse_into_gpad
 
 """
@@ -20,16 +25,21 @@ def cli():
 
 
 @cli.command()
-@click.argument("source", type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True))
-@click.option("--gpi", "-g", "gpis", multiple=True, type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True))
+@click.argument("source", type=click.Path(exists=True, readable=True, dir_okay=True, resolve_path=True))
 @click.option("--ontology", "-o", required=True, type=click.Path(exists=True, readable=True, dir_okay=False, resolve_path=True))
 @click.option("--target", "-t", required=True, type=click.Path(exists=False, resolve_path=True))
-def validate(source, gpis, ontology, target):
+def pristine(source, ontology, target):
     # parse_into_gpad.process_single_file()
     click.echo("hello")
     os.makedirs(target, exist_ok=True)
 
-    parse_into_gpad.process_single_file(source, gpis, ontology, target)
+    aligned_sources = sources.align_sources(source)  # type: List[sources.PairedSource]
+    for s in aligned_sources:
+        if not s.unmatched:
+            # If we have either a gpi with a gpad, or a gaf
+            gpi_list = [str(s.gpi)] if s.gpi else []
+            click.echo("Parsing {}".format(s.annotation))
+            parse_into_gpad.process_single_file(str(s.annotation), gpi_list, ontology, target)
 
 
 if __name__ == "__main__":
