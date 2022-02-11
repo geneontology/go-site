5c4120128b8b7629ee758edb40e7632f224e53f0
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-02-10 16:15:43 -0800

    add go_ref as a property to GoRef class

--- metadata/gorefs/parser/goref.py
+++ metadata/gorefs/parser/goref.py
@@ -1,17 +1,28 @@
+import os
 import io
 from typing import Dict, Optional, Tuple, Union
 
 import yamldown
 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 class GoRef:
-    def __init__(self, yamldown_path: str) -> None:
-        self.yd_path = yamldown_path
+    def __init__(self, goref_path: str) -> None:
+        self.yd_path = goref_path
+
+    @property
+    def goref_path(self):
+        return os.path.join(self.yd_path)
+
+    @goref_path.setter
+    def goref_path(self, goref_path: str):
+        self.yd_path = goref_path
 
     def parse(
         self, portion: Optional[str] = None
     ) -> Union[Tuple[Dict, str], Dict, str]:
-        with open(self.yd_path, "r") as file:
+        with open(self.goref_path, "r") as file:
             self.yd_content = file.read()
     
         f = io.StringIO(self.yd_content)
