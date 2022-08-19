b3c282fbfa3cf1e5b8d980daa77fa89a36afd2ee
Seth Carbon <github.com@genkisugi.net>
2022-08-18 17:28:12 -0700

    add title mode and pull up numbers; addition for #1891

--- scripts/github_issue_summary.py
+++ scripts/github_issue_summary.py
@@ -1,3 +1,12 @@
+####
+#### Examples:
+####
+#### Produce full report; aka summary email contents
+####   python3.6 ./scripts/github_issue_summary.py geneontology/go-ontology 7
+#### Produce just summary; aka summary email title
+####   python3.6 ./scripts/github_issue_summary.py geneontology/go-ontology 7 --summary_only true
+####
+
 import re
 import requests
 import json
@@ -10,7 +19,11 @@ parser.add_argument('repo_name')
 parser.add_argument('duration_in_days')
 parser.add_argument('-t', '--todays_date', help="Override the date to start 'looking back' from. Date must be in ISO "
                                                 "format e.g. '2022-08-16'")
+parser.add_argument('-s', '--summary_only', help="only output the summary; useful for titles")
 
+collected_issues = []
+new_printed_count = 0
+updated_printed_count = 0
 
 # TODO: Confirm this is working
 def make_html_safe(s):
@@ -25,26 +38,34 @@ def make_html_safe(s):
 def print_single_issue(issue):
     line = "<li><a href=\"{}\">{}</a> {}</li>".format(issue['html_url'], issue['number'],
                                                       make_html_safe(issue['title']))
-    print(line)
+    return line
 
 
-def print_issues(issues, event_type: str, printed_ids: set):
-    to_prints = []
+def collect_issues(issues, event_type: str, printed_ids: set):
+    global new_printed_count
+    global updated_printed_count
+    global collected_issues
+
     printed_count = 0
+    to_prints = []
+
     for issue in issues:
         if issue["number"] not in printed_ids:
             to_prints.append(issue)
             printed_count += 1
             printed_ids.add(issue["number"])
-    print("<h3>{} Tickets</h3>".format(event_type))
+    collected_issues.append("<h3>{} Tickets</h3>".format(event_type))
     if printed_count > 0:
-        print("There are {} {} tickets.".format(printed_count, event_type.lower()))
-        print("<ul>")
-        [print_single_issue(i) for i in to_prints]
-        print("</ul>")
+        collected_issues.append("There are {} {} tickets.".format(printed_count, event_type.lower()))
+        collected_issues.append("<ul>")
+        [collected_issues.append(print_single_issue(i)) for i in to_prints]
+        collected_issues.append("</ul>")
     else:
-        print("<p>There have been no {} tickets.</p>".format(event_type.lower()))
-
+        collected_issues.appent("<p>There have been no {} tickets.</p>".format(event_type.lower()))
+    if event_type == "New":
+        new_printed_count = printed_count
+    else:
+        updated_printed_count = printed_count
 
 def get_issues(repo: str, event_type: str, start_date: str):
     url = "https://api.github.com/search/issues?q=repo:{}+{}:=>{}+is:issue&type=Issues&per_page=100".format(repo,
@@ -82,7 +103,13 @@ if __name__ == "__main__":
     repo_name = args.repo_name
     if "/" in repo_name:
         repo_name = repo_name.rsplit("/", maxsplit=1)[-1]
-    print("<h2>Summary for tickets in {} from {} to {}</h2>".format(repo_name, yesterday, today))
     ids = set()
-    print_issues(new_issues, "New", ids)
-    print_issues(updated_issues, "Updated", ids)
+    collect_issues(new_issues, "New", ids)
+    collect_issues(updated_issues, "Updated", ids)
+
+    if args.summary_only:
+        print("Summary for tickets ({} new, {} updated) in {} from {} to {}".format(new_printed_count, updated_printed_count, repo_name, yesterday, today))
+    else:
+        print("<h2>Summary for tickets ({} new, {} updated) in {} from {} to {}</h2>".format(new_printed_count, updated_printed_count, repo_name, yesterday, today))
+        for c in collected_issues:
+            print(c)
